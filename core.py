73edb311e94fe9c85e9b080807f31729056dcffe
jaeyeon-park <parkjani2@gmail.com>
2020-03-04 10:04:55 +0900

    error handling for incorrect pannum

--- core.py
+++ core.py
@@ -25,15 +25,20 @@ def get_pan(pan_num):
     '''
     url = "{}/판례/({})".format(__BASE_URL, pan_num)
     website = __get_bs_obj(parse.quote(url)) # handle the hangeul url
-    pansrc_url = website.body.find('iframe', {'name':'lawService'})['src']
+    pansrc = website.body.find('iframe', {'name':'lawService'})
+    if pansrc is None:
+        return Pan(None, None, None, None, pan_num)
 
-    website = __get_bs_obj(os.path.join(__BASE_URL, pansrc_url))
-    contents = website.body.find_all('p',{'class':'pty4'})
+    pansrc_url = pansrc['src']
+    website = __get_bs_obj(__BASE_URL+"/"+pansrc_url)
+    contents = website.body.find_all('p', {'class':'pty4'})
     contents = [c.text for c in contents]
+    if len(contents) != 4:
+        return Pan(None, None, None, None, pan_num)
     contents.append(pan_num)
     return Pan(*contents)
 
-def save_pans(pans, ofilename):
+def save_pans(pans, ofilename, p_check=True, y_check=True, j_check=True):
     '''
     pans (list of Pan objects)
     ofilename (str) : file path to save pans as docx file
@@ -41,10 +46,17 @@ def save_pans(pans, ofilename):
     doc = Document()
     for pan in pans:
         doc.add_heading(pan.num, level=1)
-        doc.add_heading("판시사항", level=2)
-        doc.add_paragraph(pan.pansi)
-        doc.add_paragraph(pan.pansi)
-        doc.add_heading("판결요지", level=2)
-        doc.add_paragraph(pan.yozi)
+        if pan.pansi or pan.yozi or pan.jomun:
+            if p_check and pan.pansi:
+                doc.add_heading("판시사항", level=2)
+                doc.add_paragraph(pan.pansi)
+            if y_check and pan.yozi:
+                doc.add_heading("판결요지", level=2)
+                doc.add_paragraph(pan.yozi)
+            if j_check and pan.jomun:
+                doc.add_heading("참조조문", level=2)
+                doc.add_paragraph(pan.jomun)
+        else:
+            doc.add_heading("판시사항, 판결요지, 참조조문 없습니다", level=2)
     doc.save(ofilename)
     print("save: {}".format(os.path.abspath(ofilename)))
